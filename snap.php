9b936c11fe3e7438ff50f859aac5a56c4b6081e6
Dietrich Moerman <dietrich.moerman@gmail.com>
2015-04-28 13:50:37 +0200

    Use spaces, remove headers.

--- snap.php
+++ snap.php
@@ -1,30 +1,28 @@
 <?php
 
 $images = array(
-	'gang' => array(
-		'name' => 'Gang',
-		'url' => 'http://192.168.1.15/snapshot.cgi?user=anon&pwd=anon',
-	),
-	'garage' => array(
-		'name' => 'Garage',
-		'url' => 'http://192.168.1.16/snapshot.cgi?user=anon&pwd=anon',
-	),
+    'gang' => array(
+        'name' => 'Gang',
+        'url' => 'http://192.168.1.15/snapshot.cgi?user=anon&pwd=anon',
+    ),
+    'garage' => array(
+        'name' => 'Garage',
+        'url' => 'http://192.168.1.16/snapshot.cgi?user=anon&pwd=anon',
+    ),
 );
 
 if (!empty($_GET['image']) && isset($images[$_GET['image']])) {
-	$url = $images[$_GET['image']]['url'];
-	header('Content-Type: image/jpeg');
+    $url = $images[$_GET['image']]['url'];
+    header('Content-Type: image/jpeg');
 
-	$c = curl_init($url);
-	curl_exec($c);
-	curl_close($c);
+    $c = curl_init($url);
+    curl_exec($c);
+    curl_close($c);
 
-	exit;
+    exit;
 }
 
 foreach ($images as $id => $image) {
-	$url = $_SERVER['PHP_SELF'] . '?image=' . $id . '&t=' . time();
-
-	echo '<h1>' . $image['name'] . '</h1>';
-	echo '<p><img src="' . $url . '" name="' . $image['name'] . '" /></p>';
+    $url = $_SERVER['PHP_SELF'] . '?image=' . $id . '&t=' . time();
+    echo '<p><img src="' . $url . '" name="' . $image['name'] . '" /></p>';
 }
