92d32a93c1e347b2380a6725a428e87ceb472c9c
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-27 21:57:11 +0100

    Load auto wiring container from League

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+use League\Container\Container;
+use League\Container\ReflectionContainer;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+$container->delegate(new ReflectionContainer());
