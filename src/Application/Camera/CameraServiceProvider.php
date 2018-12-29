6a5eb5b6a66f5aa45a4b8b4b7f8320eaa69d128f
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 17:15:00 +0100

    Add service provider to create in memory camera repository

---
+++ src/Application/Camera/CameraServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Detroit\Cctv\Application\Camera;
+
+use Detroit\Cctv\Domain\Camera\Camera;
+use Detroit\Cctv\Domain\Camera\CameraRepository;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Uri\Uri;
+
+final class CameraServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        CameraRepository::class,
+    ];
+
+    public function register(): void
+    {
+        $this->getContainer()->share(CameraRepository::class, function () {
+            return new InMemoryCameraRepository([
+                new Camera(
+                    'Gang',
+                    Uri::createFromString('http://192.168.1.15/snapshot.cgi?user=anon&pwd=anon')
+                ),
+                new Camera(
+                    'Garage',
+                    Uri::createFromString('http://192.168.1.16/snapshot.cgi?user=anon&pwd=anon')
+                ),
+            ]);
+        });
+    }
+}
