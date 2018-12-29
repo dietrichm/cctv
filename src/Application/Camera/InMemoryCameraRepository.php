0654a9faee66d0db748a45c5aa883d4724a223da
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 16:56:51 +0100

    Create repository with cameras and return them

--- src/Application/Camera/InMemoryCameraRepository.php
+++ src/Application/Camera/InMemoryCameraRepository.php
@@ -2,12 +2,26 @@
 
 namespace Detroit\Cctv\Application\Camera;
 
+use Detroit\Cctv\Domain\Camera\Camera;
 use Detroit\Cctv\Domain\Camera\CameraRepository;
 
 class InMemoryCameraRepository implements CameraRepository
 {
+    /**
+     * @var Camera[]
+     */
+    private $cameras;
+
+    /**
+     * @param Camera[] $cameras
+     */
+    public function __construct(array $cameras)
+    {
+        $this->cameras = $cameras;
+    }
+
     public function findAll(): array
     {
-        return [];
+        return $this->cameras;
     }
 }
