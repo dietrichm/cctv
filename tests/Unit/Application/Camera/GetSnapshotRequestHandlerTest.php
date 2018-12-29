ca8be4f96af9041a0bfdc72207c36e5cf4e705d6
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 20:59:12 +0100

    Assert offline JPEG image is returned on any client exception

--- tests/Unit/Application/Camera/GetSnapshotRequestHandlerTest.php
+++ tests/Unit/Application/Camera/GetSnapshotRequestHandlerTest.php
@@ -8,6 +8,7 @@ use Detroit\Cctv\Domain\Camera\CameraNotFound;
 use Detroit\Cctv\Domain\Camera\CameraRepository;
 use Detroit\Cctv\Tests\CreatesRequests;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use League\Uri\Uri;
 use PHPUnit\Framework\TestCase;
@@ -111,5 +112,42 @@ final class GetSnapshotRequestHandlerTest extends TestCase
 
         $this->assertEquals(StatusCode::NOT_FOUND, $response->getStatusCode());
         $this->assertEmpty((string) $response->getBody());
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsOfflineImageWhenFailingToGetSnapshot()
+    {
+        $cameraName = 'foo';
+
+        $camera = new Camera(
+            $cameraName,
+            Uri::createFromString('http://example.org')
+        );
+
+        $this->cameraRepository->method('findByName')
+            ->willReturn($camera);
+
+        $this->httpClient->method('request')
+            ->willThrowException(new ClientException(
+                'error',
+                $this->createRequest()
+            ));
+
+        $response = $this->handler->__invoke(
+            $this->createRequest(),
+            new Response(),
+            ['cameraName' => $cameraName]
+        );
+
+        $this->assertEquals(
+            'image/jpeg',
+            $response->getHeaderLine('Content-Type')
+        );
+        $this->assertEquals(
+            file_get_contents($this->offlineImagePath),
+            (string) $response->getBody()
+        );
     }
 }
