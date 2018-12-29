da76765cf8f07bb60f55778bf8a8609a4f454c0d
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 18:04:59 +0100

    Assert snapshots request handler returns nothing

---
+++ tests/Unit/Application/Camera/SnapshotsRequestHandlerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Detroit\Cctv\Tests\Unit\Application\Camera;
+
+use Detroit\Cctv\Application\Camera\SnapshotsRequestHandler;
+use Detroit\Cctv\Domain\Camera\Camera;
+use Detroit\Cctv\Domain\Camera\CameraRepository;
+use Detroit\Cctv\Tests\CreatesRequests;
+use League\Uri\Uri;
+use PHPUnit\Framework\TestCase;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+final class SnapshotsRequestHandlerTest extends TestCase
+{
+    use CreatesRequests;
+
+    /**
+     * @var SnapshotsRequestHandler
+     */
+    private $handler;
+
+    /**
+     * @var MockObject
+     */
+    private $cameraRepository;
+
+    public function setUp()
+    {
+        $this->cameraRepository = $this->createMock(CameraRepository::class);
+
+        $this->handler = new SnapshotsRequestHandler(
+            $this->cameraRepository
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itRendersSnapshotsForAllCameras()
+    {
+        $response = $this->handler->__invoke(
+            $this->createRequest(),
+            new Response()
+        );
+
+        $this->assertEmpty((string) $response->getBody());
+    }
+}
