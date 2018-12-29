ceb7010aa45c8b530a2d4a36e8138ab7506b6255
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 18:06:00 +0100

    Map root route to snapshots request handler

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Detroit\Cctv\Application\Camera\CameraServiceProvider;
+use Detroit\Cctv\Application\Camera\SnapshotsRequestHandler;
 use Jenssegers\Lean\App;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -8,5 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new App();
 
 $app->getContainer()->addServiceProvider(CameraServiceProvider::class);
+
+$app->get('/', SnapshotsRequestHandler::class);
 
 $app->run();
