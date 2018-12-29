49c4a198a4ddf086058727cf945a016615d2a64b
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 18:05:29 +0100

    Implement snapshots request handler that returns nothing

---
+++ src/Application/Camera/SnapshotsRequestHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Detroit\Cctv\Application\Camera;
+
+use Detroit\Cctv\Domain\Camera\CameraRepository;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class SnapshotsRequestHandler
+{
+    /**
+     * @var CameraRepository
+     */
+    private $cameraRepository;
+
+    public function __construct(CameraRepository $cameraRepository)
+    {
+        $this->cameraRepository = $cameraRepository;
+    }
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface {
+        return $response;
+    }
+}
