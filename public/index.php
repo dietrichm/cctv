46bc6f68fda5cdda313a56416ca14d71c9de381e
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-29 17:15:29 +0100

    Load Camera service provider

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
 
+use Detroit\Cctv\Application\Camera\CameraServiceProvider;
 use Jenssegers\Lean\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new App();
+
+$app->getContainer()->addServiceProvider(CameraServiceProvider::class);
+
 $app->run();
