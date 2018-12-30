0b66a8ec114d3f01d318e5c4185fb2723c1cbeec
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-30 13:43:18 +0100

    Load the template service provider

--- public/index.php
+++ public/index.php
@@ -3,12 +3,14 @@
 use Detroit\Cctv\Application\Camera\CameraServiceProvider;
 use Detroit\Cctv\Application\Camera\GetSnapshotRequestHandler;
 use Detroit\Cctv\Application\Camera\ListSnapshotsRequestHandler;
+use Detroit\Cctv\Infrastructure\Http\TemplateServiceProvider;
 use Jenssegers\Lean\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new App();
 
+$app->getContainer()->addServiceProvider(TemplateServiceProvider::class);
 $app->getContainer()->addServiceProvider(CameraServiceProvider::class);
 
 $app->get('/', ListSnapshotsRequestHandler::class);
