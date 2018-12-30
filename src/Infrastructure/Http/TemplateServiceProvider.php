f36b6fabd5fd7ecc0dbb3ebf85c4e70693f5ee08
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-30 14:20:45 +0100

    Register Slim Twig extensions
    Now we can use the routing Twig helper function.

--- src/Infrastructure/Http/TemplateServiceProvider.php
+++ src/Infrastructure/Http/TemplateServiceProvider.php
@@ -3,7 +3,10 @@
 namespace Detroit\Cctv\Infrastructure\Http;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use Slim\Http\Uri;
+use Slim\Interfaces\RouterInterface;
 use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
 
 final class TemplateServiceProvider extends AbstractServiceProvider
 {
@@ -17,12 +20,21 @@ final class TemplateServiceProvider extends AbstractServiceProvider
     public function register(): void
     {
         $this->getContainer()->share(Twig::class, function () {
-            return new Twig(
+            $view = new Twig(
                 __DIR__ . '/../../../resources/templates',
                 [
                     'cache' => false,
                 ]
             );
+
+            $view->addExtension(new TwigExtension(
+                $this->getContainer()->get(RouterInterface::class),
+                Uri::createFromEnvironment(
+                    $this->getContainer()->get('environment')
+                )
+            ));
+
+            return $view;
         });
     }
 }
