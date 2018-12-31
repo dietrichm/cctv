4797cea4dae7f42a12e2ff10f736ca52a5a3edfc
Dietrich Moerman <dietrich.moerman@gmail.com>
2018-12-31 18:41:19 +0100

    Return offline image when the camera is unavailable

--- src/Application/Camera/SnapshotUnavailableMiddleware.php
+++ src/Application/Camera/SnapshotUnavailableMiddleware.php
@@ -2,17 +2,35 @@
 
 namespace Detroit\Cctv\Application\Camera;
 
+use Detroit\Cctv\Domain\Camera\CameraUnavailable;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 final class SnapshotUnavailableMiddleware
 {
+    /**
+     * @var string
+     */
+    private $offlineImagePath;
+
+    public function __construct(string $offlineImagePath)
+    {
+        $this->offlineImagePath = $offlineImagePath;
+    }
+
     public function __invoke(
         ServerRequestInterface $request,
         ResponseInterface $response,
         callable $next
     ): ResponseInterface {
-        $response = $next($request, $response);
+        try {
+            $response = $next($request, $response);
+        } catch (CameraUnavailable $exception) {
+            $offlineImage = file_get_contents($this->offlineImagePath);
+            $response->getBody()->write($offlineImage);
+
+            return $response->withHeader('Content-Type', 'image/jpeg');
+        }
 
         return $response;
     }
