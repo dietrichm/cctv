612706bb4dde54d27681d12038b86ae0d658b068
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-02 20:24:19 +0100

    Cache router in temporary file

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use Detroit\Cctv\Infrastructure\FilesystemServiceProvider;
 use Detroit\Cctv\Infrastructure\TwigServiceProvider;
 use Dotenv\Dotenv;
 use Jenssegers\Lean\App;
+use Slim\Settings;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -15,6 +16,11 @@ $dotenv = new Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
 $app = new App();
+
+$app->getContainer()->get(Settings::class)->set(
+    'routerCacheFile',
+    getenv('TMP_DIR') . '/router-cache.php'
+);
 
 $app->getContainer()->addServiceProvider(FilesystemServiceProvider::class);
 $app->getContainer()->addServiceProvider(TwigServiceProvider::class);
