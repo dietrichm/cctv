b551784eef29fecc7734467bebda5a5c6880309d
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-06 22:00:34 +0100

    Add dummy reboot camera command handler

---
+++ src/Application/Camera/RebootCameraHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Detroit\Cctv\Application\Camera;
+
+use Detroit\Cctv\Domain\Camera\RebootCameraCommand;
+
+final class RebootCameraHandler
+{
+    public function handleRebootCameraCommand(RebootCameraCommand $command)
+    {
+    }
+}
