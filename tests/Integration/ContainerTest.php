99aee003d7790f86ce93cb517e9a6254f8e8dec5
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-06 17:29:20 +0100

    Add dummy integration test to assert shared dependencies in container

---
+++ tests/Integration/ContainerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Detroit\Cctv\Tests\Integration;
+
+use Detroit\Cctv\Application\Camera\CameraServiceProvider;
+use Detroit\Cctv\Infrastructure\CommandBusServiceProvider;
+use Detroit\Cctv\Infrastructure\FilesystemServiceProvider;
+use Detroit\Cctv\Infrastructure\TwigServiceProvider;
+use League\Container\Container;
+use League\Container\ReflectionContainer;
+use PHPUnit\Framework\TestCase;
+
+final class ContainerTest extends TestCase
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = new Container();
+        $this->container->delegate(new ReflectionContainer());
+
+        $this->container->addServiceProvider(CameraServiceProvider::class);
+        $this->container->addServiceProvider(CommandBusServiceProvider::class);
+        $this->container->addServiceProvider(FilesystemServiceProvider::class);
+        $this->container->addServiceProvider(TwigServiceProvider::class);
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsInstanceOfSharedDependency()
+    {
+        $this->markTestIncomplete();
+    }
+}
