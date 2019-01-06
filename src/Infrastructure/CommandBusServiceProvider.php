921202375837dde67c13505620c799331bef85ee
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-06 16:48:13 +0100

    Configure command bus with handler middleware
    See http://tactician.thephpleague.com/tweaking-tactician/

--- src/Infrastructure/CommandBusServiceProvider.php
+++ src/Infrastructure/CommandBusServiceProvider.php
@@ -4,6 +4,10 @@ namespace Detroit\Cctv\Infrastructure;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Tactician\CommandBus;
+use League\Tactician\Container\ContainerLocator;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\MethodNameInflector\HandleClassNameInflector;
 
 final class CommandBusServiceProvider extends AbstractServiceProvider
 {
@@ -17,7 +21,16 @@ final class CommandBusServiceProvider extends AbstractServiceProvider
     public function register(): void
     {
         $this->getContainer()->share(CommandBus::class, function () {
-            return new CommandBus([]);
+            return new CommandBus([
+                new CommandHandlerMiddleware(
+                    new ClassNameExtractor(),
+                    new ContainerLocator(
+                        $this->getContainer(),
+                        []
+                    ),
+                    new HandleClassNameInflector()
+                ),
+            ]);
         });
     }
 }
