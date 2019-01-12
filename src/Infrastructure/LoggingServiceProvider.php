e0d603fddadcf17bad3d155b2cbf670bec5dee33
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-12 13:26:32 +0100

    Share LoggerInterface as Monolog writing to log file

--- src/Infrastructure/LoggingServiceProvider.php
+++ src/Infrastructure/LoggingServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Detroit\Cctv\Infrastructure;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 
 final class LoggingServiceProvider extends AbstractServiceProvider
 {
@@ -10,9 +13,16 @@ final class LoggingServiceProvider extends AbstractServiceProvider
      * @var array
      */
     protected $provides = [
+        LoggerInterface::class,
     ];
 
     public function register(): void
     {
+        $this->getContainer()->share(LoggerInterface::class, function () {
+            $logger = new Logger('cctv');
+            $logger->pushHandler(new StreamHandler(getenv('LOG_FILE')));
+
+            return $logger;
+        });
     }
 }
