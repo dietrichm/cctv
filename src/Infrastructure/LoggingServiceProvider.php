1c41d4794d26ae142da476600f92779b8f0b700c
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-12 15:07:43 +0100

    Push Raven handler if DSN is provided.

--- src/Infrastructure/LoggingServiceProvider.php
+++ src/Infrastructure/LoggingServiceProvider.php
@@ -3,9 +3,12 @@
 namespace Detroit\Cctv\Infrastructure;
 
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RavenHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
+use Raven_Client;
 
 final class LoggingServiceProvider extends AbstractServiceProvider
 {
@@ -22,9 +25,18 @@ final class LoggingServiceProvider extends AbstractServiceProvider
             $logger = new Logger('cctv');
 
             $logFile = getenv('LOG_FILE');
+            $ravenDsn = getenv('RAVEN_DSN');
 
             if ($logFile !== false) {
                 $logger->pushHandler(new StreamHandler($logFile));
+            }
+
+            if ($ravenDsn !== false) {
+                $ravenHandler = new RavenHandler(new Raven_Client($ravenDsn));
+                $ravenHandler->setFormatter(
+                    new LineFormatter("%message% %context% %extra%\n")
+                );
+                $logger->pushHandler($ravenHandler);
             }
 
             return $logger;
