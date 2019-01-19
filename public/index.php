4701b23deb920febb40b24bfb26f30274ed4a53c
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-19 19:59:20 +0100

    Disable Lean method param injection

--- public/index.php
+++ public/index.php
@@ -28,6 +28,10 @@ $app->getContainer()->get(Settings::class)->set(
     'routerCacheFile',
     getenv('TMP_DIR') . '/router-cache.php'
 );
+$app->getContainer()->get(Settings::class)->set(
+    'methodInjection',
+    false
+);
 
 $app->getContainer()->addServiceProvider(CameraServiceProvider::class);
 $app->getContainer()->addServiceProvider(CommandBusServiceProvider::class);
