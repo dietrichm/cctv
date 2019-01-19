e5ec81ff03a229ca74f8ee5413ad75e4b7fa33e6
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-19 20:28:21 +0100

    Remove service providers and config from index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,10 @@
 <?php
 
 use Detroit\Cctv\Application\App;
-use Detroit\Cctv\Application\Camera\CameraServiceProvider;
 use Detroit\Cctv\Application\Camera\GetSnapshotRequestHandler;
 use Detroit\Cctv\Application\Camera\ListSnapshotsRequestHandler;
 use Detroit\Cctv\Application\Camera\SnapshotUnavailableMiddleware;
-use Detroit\Cctv\Infrastructure\CommandBusServiceProvider;
-use Detroit\Cctv\Infrastructure\FilesystemServiceProvider;
-use Detroit\Cctv\Infrastructure\LoggingServiceProvider;
-use Detroit\Cctv\Infrastructure\TwigServiceProvider;
 use Dotenv\Dotenv;
-use Monolog\ErrorHandler;
-use Psr\Log\LoggerInterface;
-use Slim\Settings;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -20,19 +12,6 @@ $dotenv = new Dotenv(__DIR__ . '/..');
 $dotenv->overload();
 
 $app = new App();
-
-$app->getContainer()->addServiceProvider(LoggingServiceProvider::class);
-ErrorHandler::register($app->getContainer()->get(LoggerInterface::class));
-
-$app->getContainer()->get(Settings::class)->set(
-    'routerCacheFile',
-    getenv('TMP_DIR') . '/router-cache.php'
-);
-
-$app->getContainer()->addServiceProvider(CameraServiceProvider::class);
-$app->getContainer()->addServiceProvider(CommandBusServiceProvider::class);
-$app->getContainer()->addServiceProvider(FilesystemServiceProvider::class);
-$app->getContainer()->addServiceProvider(TwigServiceProvider::class);
 
 $app->get('/', ListSnapshotsRequestHandler::class);
 $app->get('/snapshot/{cameraName}', GetSnapshotRequestHandler::class)
