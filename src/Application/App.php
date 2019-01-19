4a436379d4a78999b0c57aa7400fad7eb9fcf761
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-19 20:26:55 +0100

    Add service providers and config as done in index.php

--- src/Application/App.php
+++ src/Application/App.php
@@ -2,8 +2,33 @@
 
 namespace Detroit\Cctv\Application;
 
+use Detroit\Cctv\Application\Camera\CameraServiceProvider;
+use Detroit\Cctv\Infrastructure\CommandBusServiceProvider;
+use Detroit\Cctv\Infrastructure\FilesystemServiceProvider;
+use Detroit\Cctv\Infrastructure\LoggingServiceProvider;
+use Detroit\Cctv\Infrastructure\TwigServiceProvider;
 use Jenssegers\Lean\App as LeanApp;
+use Monolog\ErrorHandler;
+use Psr\Log\LoggerInterface;
+use Slim\Settings;
 
 class App extends LeanApp
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->getContainer()->addServiceProvider(LoggingServiceProvider::class);
+        ErrorHandler::register($this->getContainer()->get(LoggerInterface::class));
+
+        $this->getContainer()->get(Settings::class)->set(
+            'routerCacheFile',
+            getenv('TMP_DIR') . '/router-cache.php'
+        );
+
+        $this->getContainer()->addServiceProvider(CameraServiceProvider::class);
+        $this->getContainer()->addServiceProvider(CommandBusServiceProvider::class);
+        $this->getContainer()->addServiceProvider(FilesystemServiceProvider::class);
+        $this->getContainer()->addServiceProvider(TwigServiceProvider::class);
+    }
 }
