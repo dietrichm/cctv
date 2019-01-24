2c8ba3c58f0c6aa0a7e36ce4544a8be364d6ddd0
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-24 20:26:15 +0100

    Assert reboot cameras command is configured correctly

--- tests/Unit/Application/Camera/RebootCamerasCommandTest.php
+++ tests/Unit/Application/Camera/RebootCamerasCommandTest.php
@@ -2,8 +2,27 @@
 
 namespace Detroit\Cctv\Tests\Unit\Application\Camera;
 
+use Detroit\Cctv\Application\Camera\RebootCamerasCommand;
 use PHPUnit\Framework\TestCase;
 
 final class RebootCamerasCommandTest extends TestCase
 {
+    /**
+     * @var RebootCamerasCommand
+     */
+    private $command;
+
+    public function setUp()
+    {
+        $this->command = new RebootCamerasCommand();
+    }
+
+    /**
+     * @test
+     */
+    public function itIsConfiguredCorrectly()
+    {
+        $this->assertEquals('reboot-cameras', $this->command->getName());
+        $this->assertEquals('Reboot all cameras', $this->command->getDescription());
+    }
 }
