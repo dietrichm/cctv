58045460edfcabeb0fda1e85a81b45abfc37f3c2
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-24 22:38:39 +0100

    Inject camera repository into command

--- src/Application/Camera/RebootCamerasCommand.php
+++ src/Application/Camera/RebootCamerasCommand.php
@@ -2,13 +2,26 @@
 
 namespace Detroit\Cctv\Application\Camera;
 
+use Detroit\Cctv\Domain\Camera\CameraRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class RebootCamerasCommand extends Command
 {
+    /**
+     * @var CameraRepository
+     */
+    private $cameraRepository;
+
     protected static $defaultName = 'reboot-cameras';
+
+    public function __construct(CameraRepository $cameraRepository)
+    {
+        parent::__construct();
+
+        $this->cameraRepository = $cameraRepository;
+    }
 
     protected function configure(): void
     {
--- tests/Unit/Application/Camera/RebootCamerasCommandTest.php
+++ tests/Unit/Application/Camera/RebootCamerasCommandTest.php
@@ -3,6 +3,7 @@
 namespace Detroit\Cctv\Tests\Unit\Application\Camera;
 
 use Detroit\Cctv\Application\Camera\RebootCamerasCommand;
+use Detroit\Cctv\Domain\Camera\CameraRepository;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Output\Output;
@@ -10,13 +11,21 @@ use Symfony\Component\Console\Output\Output;
 final class RebootCamerasCommandTest extends TestCase
 {
     /**
+     * @var MockObject
+     */
+    private $cameraRepository;
+
+    /**
      * @var RebootCamerasCommand
      */
     private $command;
 
     public function setUp()
     {
-        $this->command = new RebootCamerasCommand();
+        $this->cameraRepository = $this->createMock(CameraRepository::class);
+        $this->command = new RebootCamerasCommand(
+            $this->cameraRepository
+        );
     }
 
     /**
