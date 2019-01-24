f41ee12d214279ad1fc2c334d349e2bef008bb6e
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-24 20:11:09 +0100

    Add dummy console command to reboot all cameras

---
+++ src/Application/Camera/RebootCamerasCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Detroit\Cctv\Application\Camera;
+
+use Symfony\Component\Console\Command\Command;
+
+final class RebootCamerasCommand extends Command
+{
+}
---
+++ tests/Unit/Application/Camera/RebootCamerasCommandTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Detroit\Cctv\Tests\Unit\Application\Camera;
+
+use PHPUnit\Framework\TestCase;
+
+final class RebootCamerasCommandTest extends TestCase
+{
+}
