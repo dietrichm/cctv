c5d32586f05bb088bdc2de1d52f4970bcd9c2075
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-24 20:26:42 +0100

    Configure reboot cameras command so it can be loaded

--- src/Application/Camera/RebootCamerasCommand.php
+++ src/Application/Camera/RebootCamerasCommand.php
@@ -6,4 +6,10 @@ use Symfony\Component\Console\Command\Command;
 
 final class RebootCamerasCommand extends Command
 {
+    protected static $defaultName = 'reboot-cameras';
+
+    protected function configure(): void
+    {
+        $this->setDescription('Reboot all cameras');
+    }
 }
