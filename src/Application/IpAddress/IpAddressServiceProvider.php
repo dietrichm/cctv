ac573d641bfc371f51dfd69d9afaba0efd986618
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-27 21:32:10 +0100

    Provide HttpPublicIpAddressReader as PublicIpAddressReader

---
+++ src/Application/IpAddress/IpAddressServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Detroit\Cctv\Application\IpAddress;
+
+use Detroit\Cctv\Domain\IpAddress\PublicIpAddressReader;
+use GuzzleHttp\Client;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+final class IpAddressServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        PublicIpAddressReader::class,
+    ];
+
+    public function register(): void
+    {
+        $this->getContainer()->share(PublicIpAddressReader::class, function () {
+            return new HttpPublicIpAddressReader(
+                new Client(),
+                'https://api.ipify.org'
+            );
+        });
+    }
+}
