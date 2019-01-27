77c24b92536c9df768287490dd3dc0597a4aac76
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-27 18:46:26 +0100

    Assert no invalid IP address can be created

--- tests/Unit/Domain/IpAddress/IpAddressTest.php
+++ tests/Unit/Domain/IpAddress/IpAddressTest.php
@@ -3,6 +3,7 @@
 namespace Detroit\Cctv\Tests\Unit\Domain\IpAddress;
 
 use Detroit\Cctv\Domain\IpAddress\IpAddress;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 final class IpAddressTest extends TestCase
@@ -23,6 +24,33 @@ final class IpAddressTest extends TestCase
         return [
             ['250.23.235.22'],
             ['5ab7:c057:1ecd:ace9:9e14:9e1:7b21:b013'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider providesInvalidIpAddresses
+     */
+    public function itDoesNotCreateInvalidIpAddress(string $ipAddress)
+    {
+        $this->expectExceptionObject(
+            new InvalidArgumentException('Invalid IP address ' . $ipAddress)
+        );
+
+        new IpAddress($ipAddress);
+    }
+
+    public function providesInvalidIpAddresses(): array
+    {
+        return [
+            ['250.23.235.999'],
+            ['5ab7:c057:1ecd:ace9:9e14:9z1:7b21:b013'],
+            ['250.23.235'],
+            ['5ab7:c057:1ecd:ace9:9e14:9z1:7b21'],
+            ['some string'],
+            [12345],
+            [12.34],
+            [true],
         ];
     }
 }
