79f01488feb7acb88cbdd91dca2f149f1e494381
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-27 21:05:40 +0100

    Assert IpAddressReadFailed is thrown when API is down

--- tests/Unit/Application/IpAddress/IpifyPublicIpAddressReaderTest.php
+++ tests/Unit/Application/IpAddress/IpifyPublicIpAddressReaderTest.php
@@ -4,13 +4,18 @@ namespace Detroit\Cctv\Tests\Unit\Application\IpAddress;
 
 use Detroit\Cctv\Application\IpAddress\IpifyPublicIpAddressReader;
 use Detroit\Cctv\Domain\IpAddress\IpAddress;
+use Detroit\Cctv\Domain\IpAddress\IpAddressReadFailed;
+use Detroit\Cctv\Tests\CreatesRequests;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 final class IpifyPublicIpAddressReaderTest extends TestCase
 {
+    use CreatesRequests;
+
     /**
      * @var MockObject
      */
@@ -50,5 +55,21 @@ final class IpifyPublicIpAddressReaderTest extends TestCase
             new IpAddress('160.237.142.139'),
             $this->reader->get()
         );
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsWhenFailingToGetIpAddress()
+    {
+        $this->httpClient->method('request')
+            ->willThrowException(new RequestException(
+                'error',
+                $this->createRequest()
+            ));
+
+        $this->expectException(IpAddressReadFailed::class);
+
+        $this->reader->get();
     }
 }
