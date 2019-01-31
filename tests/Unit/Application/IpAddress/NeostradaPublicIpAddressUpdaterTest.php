815768d971f28085f92423d4ca374e62fcc05510
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-31 19:35:39 +0100

    Assert updater calls API endpoint with DNS ID

--- tests/Unit/Application/IpAddress/NeostradaPublicIpAddressUpdaterTest.php
+++ tests/Unit/Application/IpAddress/NeostradaPublicIpAddressUpdaterTest.php
@@ -3,7 +3,9 @@
 namespace Detroit\Cctv\Tests\Unit\Application\IpAddress;
 
 use Detroit\Cctv\Application\IpAddress\NeostradaPublicIpAddressUpdater;
+use Detroit\Cctv\Domain\IpAddress\IpAddress;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -29,5 +31,23 @@ final class NeostradaPublicIpAddressUpdaterTest extends TestCase
             303,
             808
         );
+    }
+
+    /**
+     * @test
+     */
+    public function itUpdatesIpAddressOnDnsRecord()
+    {
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                'patch',
+                'https://api.neostrada.com/api/dns/edit/303'
+            )
+            ->willReturn(new Response());
+
+        $this->updater->set(new IpAddress('179.26.203.196'));
+
+        $this->markTestIncomplete();
     }
 }
