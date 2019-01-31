236b1ed47849577f58dde925d835e7fd2a924fcd
Dietrich Moerman <dietrich.moerman@gmail.com>
2019-01-31 19:29:20 +0100

    Inject HTTP client and call info from constructor

--- src/Application/IpAddress/NeostradaPublicIpAddressUpdater.php
+++ src/Application/IpAddress/NeostradaPublicIpAddressUpdater.php
@@ -4,9 +4,42 @@ namespace Detroit\Cctv\Application\IpAddress;
 
 use Detroit\Cctv\Domain\IpAddress\IpAddress;
 use Detroit\Cctv\Domain\IpAddress\PublicIpAddressUpdater;
+use GuzzleHttp\Client;
 
 final class NeostradaPublicIpAddressUpdater implements PublicIpAddressUpdater
 {
+    /**
+     * @var Client
+     */
+    private $httpClient;
+
+    /**
+     * @var string
+     */
+    private $apiToken;
+
+    /**
+     * @var int
+     */
+    private $dnsId;
+
+    /**
+     * @var int
+     */
+    private $recordId;
+
+    public function __construct(
+        Client $httpClient,
+        string $apiToken,
+        int $dnsId,
+        int $recordId
+    ) {
+        $this->httpClient = $httpClient;
+        $this->apiToken = $apiToken;
+        $this->dnsId = $dnsId;
+        $this->recordId = $recordId;
+    }
+
     public function set(IpAddress $ipAddress): void
     {
     }
